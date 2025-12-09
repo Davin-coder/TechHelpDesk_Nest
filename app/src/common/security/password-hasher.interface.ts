9d2f079634b03ce938e5f1330e50d4df66c1561e
Davin <alextuiran8@gmail.com>
2025-12-09 09:35:35 -0500

    feat(user): Added passwordHash service

---
+++ app/src/common/security/password-hasher.interface.ts
@@ -0,0 +1,6 @@
+export const PASSWORD_HASHER = 'PASSWORD_HASHER';
+
+export interface IPasswordHasher {
+    hash(plain: string): Promise<string>;
+    compare(plain: string, hash: string): Promise<boolean>;
+}
